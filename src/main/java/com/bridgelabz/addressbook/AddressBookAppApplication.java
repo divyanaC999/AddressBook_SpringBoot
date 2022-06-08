a6a5ab0900a23a09a6ca8fff3f277e975fc2a6b4
divyanaC999 <divyachourasiya999@gmail.com>
2022-06-09 02:55:27 +0530

    Added:Create AddressBook Spring project

---
+++ src/main/java/com/bridgelabz/addressbook/AddressBookAppApplication.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.addressbook;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookAppApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book");
+        SpringApplication.run(AddressBookAppApplication.class, args);
+    }
+
+}
